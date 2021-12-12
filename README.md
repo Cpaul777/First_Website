ad224881c710ff3fe152f644d598b7994027fd5c
fREd <fredericktulba9@gmail.com>
2021-12-12 15:05:05 +0800

    added wassup hoomans

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is first website using python and flask
 
 the files are already in here.
+
+Wassup hoomans
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from website import first_app
 
 app = first_app()
 
+#beaksididid 
+
 if __name__ == '__main__':
     app.run(debug=True)
 

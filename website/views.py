1834d9f67d94f15c4f9fb5a46426a14f414edb24
fREd <fredericktulba9@gmail.com>
2021-12-20 19:03:11 +0800

    removed chat

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/home')
 def homepage():
     return render_template("home.html")
 
-@views.route('about-us')
+@views.route('/about-us')
 def aboutUs():
     return render_template("aboutUs.html", )

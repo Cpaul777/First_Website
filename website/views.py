af21757d9381747c6265ae4a62bec465270fa2a6
Cpaul777 <sabanalpaul009@gmail.com>
2022-01-05 19:25:00 +0800

    Changes and working sign up and database

--- website/views.py
+++ website/views.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 
-@views.route('/')
-def homepage():
+@views.route("/")
+def home():
     return render_template("home.html")
 
-@views.route('about-us')
+
+@views.route("about-us")
 def aboutUs():
-    return render_template("aboutUs.html", )
+    return render_template(
+        "aboutUs.html",
+    )
